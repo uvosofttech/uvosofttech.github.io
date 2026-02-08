c8d77a95bb6b87008552b6ea0273508402717ba1
uvosofttech <uvosofttech@gmail.com>
2026-02-08 21:27:44 +0500

    Delete js/app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const path = location.pathname.split("/").pop() || "index.html";
-  document.querySelectorAll('a[data-nav]').forEach(a=>{
-    const href = a.getAttribute("href");
-    if(href === path) a.classList.add("active");
-  });
-})();
